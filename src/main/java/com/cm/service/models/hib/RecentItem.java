6ff5ddd39fd987354d5e87fff046cc09f93e2b4a
Arnab Mondal <arnab2807@gmail.com>
2019-05-27 00:00:16 +0530

    Maven Conversion

--- src/main/java/com/cm/service/models/hib/RecentItem.java
+++ src/main/java/com/cm/service/models/hib/RecentItem.java
@@ -8,74 +8,74 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "cms_recent_items")
 public class RecentItem implements CMEntity {
-	public RecentItem() {
-		this.recordId = generateId("REC");
-	}
+    public RecentItem() {
+        this.recordId = generateId("REC");
+    }
 
-	@Id
-	@Column(length = 12)
-	private String recordId;
-	private String itemType;
-	private String itemName;
-	private String itemId;
-	private String changeTime;
-	private String changeType;
-	private String changeDescription;
+    @Id
+    @Column(length = 12)
+    private String recordId;
+    private String itemType;
+    private String itemName;
+    private String itemId;
+    private String changeTime;
+    private String changeType;
+    private String changeDescription;
 
-	public String getRecordId() {
-		return recordId;
-	}
+    public String getRecordId() {
+        return recordId;
+    }
 
-	public void setRecordId(String recordId) {
-		this.recordId = recordId;
-	}
+    public void setRecordId(String recordId) {
+        this.recordId = recordId;
+    }
 
-	public String getItemType() {
-		return itemType;
-	}
+    public String getItemType() {
+        return itemType;
+    }
 
-	public void setItemType(String itemType) {
-		this.itemType = itemType;
-	}
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
 
-	public String getItemName() {
-		return itemName;
-	}
+    public String getItemName() {
+        return itemName;
+    }
 
-	public void setItemName(String itemName) {
-		this.itemName = itemName;
-	}
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
 
-	public String getChangeTime() {
-		return changeTime;
-	}
+    public String getChangeTime() {
+        return changeTime;
+    }
 
-	public void setChangeTime(String changeTime) {
-		this.changeTime = changeTime;
-	}
+    public void setChangeTime(String changeTime) {
+        this.changeTime = changeTime;
+    }
 
-	public String getChangeType() {
-		return changeType;
-	}
+    public String getChangeType() {
+        return changeType;
+    }
 
-	public void setChangeType(String changeType) {
-		this.changeType = changeType;
-	}
+    public void setChangeType(String changeType) {
+        this.changeType = changeType;
+    }
 
-	public String getChangeDescription() {
-		return changeDescription;
-	}
+    public String getChangeDescription() {
+        return changeDescription;
+    }
 
-	public void setChangeDescription(String changeDescription) {
-		this.changeDescription = changeDescription;
-	}
+    public void setChangeDescription(String changeDescription) {
+        this.changeDescription = changeDescription;
+    }
 
-	public String getItemId() {
-		return itemId;
-	}
+    public String getItemId() {
+        return itemId;
+    }
 
-	public void setItemId(String itemId) {
-		this.itemId = itemId;
-	}
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
 
 }
